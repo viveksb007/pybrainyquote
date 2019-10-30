467e0733d90956d7273913511c55db38f0810b35
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2019-10-30 22:53:57 +0530

    released 0.2 on pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='brainyquote',
     packages=['brainyquote'],
-    version='0.1',
+    version='0.2',
     description='Fetches and Returns quotes from www.brainyquote.com',
     author='Vivek Singh Bhadauria',
     author_email='viveksbhadauria007@gmail.com',
     url='https://github.com/viveksb007/pybrainyquote',
-    download_url ='https://github.com/viveksb007/pybrainyquote/tarball/0.1',
+    download_url ='https://github.com/viveksb007/pybrainyquote/tarball/0.2',
     keywords = ['quotes', 'pyquotes', 'brainyquote'],
     classifiers = [],
     install_requires=[
